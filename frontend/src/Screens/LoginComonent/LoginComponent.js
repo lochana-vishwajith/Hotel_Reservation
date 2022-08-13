29d27ff1cc2fb2eec3d5164ecba12a501068678a
lochana-vishwajith <lochanavishwajith302@gmail.com>
2022-08-13 14:08:54 +0530

    login component created

--- frontend/src/Screens/LoginComonent/LoginComponent.js
+++ frontend/src/Screens/LoginComonent/LoginComponent.js
@@ -1,7 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import TextBox from "devextreme-react/text-box";
+import "./Login.css";
+import { Toast } from "devextreme-react/toast";
+import LogoImage from "../../Assets/Images/oie_8ndMsF0hLOiZ.png";
+import { Link } from "react-router-dom";
 
 const LoginComponent = () => {
-  return <div>LoginComponent</div>;
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [toastVisible, setToastVisible] = useState(false);
+  const [toastmessage, settoastmessage] = useState("");
+  const [toastType, settoastType] = useState("");
+
+  const logInUser = () => {
+    if (userName === "" || password === "") {
+      setToastVisible(true);
+      settoastmessage("Please fill all the fields");
+      settoastType("error");
+    }
+  };
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-xl-7" id="regImageCol">
+          <span className="welcomeTxt">OLIVE GARDEN RESTURANT</span>
+          <br />
+          <img src={LogoImage} className="logo" />
+          <br />
+          <center>
+            <span className="loginReqTxt">
+              Don't have an account? <Link to={"/register"}>Register</Link>
+            </span>
+          </center>
+        </div>
+        <div className="col-sm-4" id="regFormCol">
+          <div className="card" id="loginCard">
+            <label className="regFormTxt">UserName / Email</label>
+            <TextBox
+              showClearButton={true}
+              placeholder="John Smith"
+              onChange={(val) => setUserName(val)}
+            />
+            <br />
+            <span className="regFormTxt">Password</span>
+            <TextBox
+              showClearButton={true}
+              placeholder="*********"
+              onChange={(val) => setPassword(val)}
+            />
+
+            <br />
+
+            <center>
+              <Toast
+                visible={toastVisible}
+                message={toastmessage}
+                type={toastType}
+                onHiding={() => setToastVisible(false)}
+                displayTime={1500}
+              />
+              <button
+                type="button"
+                class="btn btn-outline-primary"
+                onClick={() => logInUser()}
+              >
+                Login
+              </button>
+              <br />
+              <br />
+              <span className="forgotPw">
+                <Link to="/forgotPw">Forgot Password? </Link>
+              </span>
+            </center>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginComponent;
