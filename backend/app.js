9fc9e1cbfcde27392de138ac860e65d1a27a805e
lochana-vishwajith <lochanavishwajith302@gmail.com>
2022-08-13 07:52:18 +0530

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json({ extended: false }));
+
+const PORT = process.env.PORT || 5000;
+const url = process.env.MONGODB_URL;
+
+try {
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("MongoDB connected");
+} catch (error) {
+  console.log(error.message);
+
+  //Exit Process with failure
+  process.exit(1);
+}
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
